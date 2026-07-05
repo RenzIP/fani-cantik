6d59afd35f3dc7133202d291b53855134adcf38d
Fani cantik <zenkun.enterkill13@gmail.com>
2026-07-05 20:49:57 +0700

    push

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,10 +1,10 @@
 <?php
 declare(strict_types=1);
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'nasi_bakar_rpl';
+$host = 'sao.domcloud.co';
+$user = 'nasi-bakar-rpl';
+$password = 'PxbT_1(5c769uUTgA_';
+$database = 'nasi_bakar_rpl_db';
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
